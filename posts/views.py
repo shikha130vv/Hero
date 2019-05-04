207a18697a201f140a8d1d8b6910140da536d2f9
shikha130vv <32208501+shikha130vv@users.noreply.github.com>
2019-05-04 11:33:10 +0530

    Add files via upload

--- posts/views.py
+++ posts/views.py
@@ -17,14 +17,15 @@ def post_image_view(request):
         if form.is_valid(): 
             objModel = form.save() 
             img_class = get_image_class(objModel.cover)
-            return redirect('success/' + img_class) 
+            url = objModel.cover.url
+            return HttpResponse('<img src="' + url + '"><br>' + img_class) 
     else: 
         form = PostForm() 
     return render(request, 'post_image_form.html', {'form' : form}) 
   
   
 def success(request, img_class): 
-    return HttpResponse('Hero: ' + img_class) 
+    return 
   
  
 def triplet_loss(y_true, y_pred, alpha = 0.2):
@@ -59,6 +60,7 @@ def load_data(FRmodel):
 def get_encoding(img_file, FRmodel):
     pic = Image.open(img_file)
     img = pic.resize((96,96), Image.ANTIALIAS)
+    img = np.array(img)
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     encoding = FRmodel.predict_on_batch(x_train)
@@ -70,20 +72,26 @@ def get_image_class(img_file):
     FRmodel = load_model(model_path, custom_objects={"triplet_loss":triplet_loss})
 
     encoding = get_encoding(img_file, FRmodel)
-    df = load_data(FRmodel)
-    if 1==2:
-         workpath = os.path.dirname(os.path.abspath(__file__))
-         cvs_path = os.path.join(workpath, 'model_encoding.xls')
-         df = pd.read_excel(cvs_path)
+    
+    workpath = os.path.dirname(os.path.abspath(__file__))
+    cvs_path = os.path.join(workpath, 'model_encoding.csv')
+    if 1==1:
+         df = pd.read_csv(cvs_path)
          df["fn_encoding"] = df["str_encoding"].map(lambda x:np.array(x.split("^")).reshape(1,-1).astype(np.float32))
+    else:
+         df = load_data(FRmodel)
+         df["str_encoding"] = df["fn_encoding"].map(lambda x: "^".join(list(x.flatten().astype(str))))
+         df[["class_name","str_encoding"]].to_csv(cvs_path, index=False)
 
     df["dist"] = df["fn_encoding"].apply(lambda x: np.linalg.norm(encoding- x))
-    idx = df.sort_values("dist").head(1).index
-    idx1 = df.sort_values("dist")[1:].head(1).index
-    pred_class = df.loc[idx, "class_name"].values[0]
-    pred_dist = df.loc[idx, "dist"].values[0]
-    tail_dist = df.loc[idx1, "dist"].values[0]
-    return pred_class + str(pred_dist) + str(idx) + str(tail_dist)
+    idx = df.sort_values("dist").index.values[0]
+    idx1 = df.sort_values("dist").index.values[1]
+    pred_class = df.loc[idx, "class_name"]
+    pred_dist = df.loc[idx, "dist"]
+    tail_dist = df.loc[idx1, "dist"]
+    pred_class1 = df.loc[idx, "class_name"]
+    return pred_class
+    #return "Prediction:" + pred_class + " Distance:" + str(pred_dist) + " Next Prediction:" + pred_class1 + " Distance:" + str(tail_dist)
 
 
 
